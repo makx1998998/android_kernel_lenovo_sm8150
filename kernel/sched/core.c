4c656b46ec863c785fd2783bb83784bf3cd3dd98
WhiTiger96 <max1996_996@mail.ru>
2020-04-21 09:59:47 +0300

    revert to Add SIO and FIOPS i/o schedulers

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1069,26 +1069,10 @@ void set_cpus_allowed_common(struct task_struct *p, const struct cpumask *new_ma
 	p->nr_cpus_allowed = cpumask_weight(new_mask);
 }
 
-static const struct cpumask *
-get_adjusted_cpumask(const struct task_struct *p,
-		     const struct cpumask *orig_mask)
-{
-	/* Force all performance-critical kthreads onto the big cluster */
-	if (p->flags & PF_PERF_CRITICAL)
-		return cpu_perf_mask;
-	/* Force all low-power kthreads onto the little cluster */
-	if (p->flags & PF_LOW_POWER)
-		return cpu_lp_mask;
-
-	return orig_mask;
-}
-
 void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
 {
 	struct rq *rq = task_rq(p);
 	bool queued, running;
-
-	new_mask = get_adjusted_cpumask(p, new_mask);
 
 	lockdep_assert_held(&p->pi_lock);
 
@@ -1132,8 +1116,6 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 	struct rq *rq;
 	int ret = 0;
 	cpumask_t allowed_mask;
-
-	new_mask = get_adjusted_cpumask(p, new_mask);
 
 	rq = task_rq_lock(p, &rf);
 	update_rq_clock(rq);
@@ -2498,8 +2480,6 @@ int sched_fork(unsigned long clone_flags, struct task_struct *p)
 	 * Make sure we do not leak PI boosting priority to the child.
 	 */
 	p->prio = current->normal_prio;
-
-	p->compensate_need = 0;
 
 	/*
 	 * Revert to default priority/policy on fork if requested.
---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#0 SMP PREEMPT Mon Apr 20 19:11:23 EEST 2020"
+#define LINUX_COMPILE_BY "whitiger"
+#define LINUX_COMPILE_HOST "whitiger-355V4C-356V4C-3445VC-3545VC"
+#define LINUX_COMPILER "clang version 4.0.10 for Android NDK"
